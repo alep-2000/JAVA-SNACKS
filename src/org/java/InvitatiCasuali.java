a020fa47edca6ae1e4e594eebda28a866d91b6d5
alep-2000 <alessiopiras056@gmail.com>
2023-11-10 15:14:12 +0100

    Array nomi e cognomi

--- src/org/java/InvitatiCasuali.java
+++ src/org/java/InvitatiCasuali.java
@@ -1,5 +1,6 @@
 package org.java;
 
 public class InvitatiCasuali {
-
+	String[] nomi = {"Alessio", "Giuseppe", "Francesco", "Laura", "Francesca", "Sara"};
+	String[] cognomi = {"Palmas", "Piras", "Cordeddu", "Lippi", "Santi", "Vadilonga"};
 }
