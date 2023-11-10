13d1ad0a42ee63f61e0626843d39aa09aa78298e
alep-2000 <alessiopiras056@gmail.com>
2023-11-10 15:09:39 +0100

    Richiesta numero all'utente

--- src/org/java/NumeroPariDispari.java
+++ src/org/java/NumeroPariDispari.java
@@ -1,5 +1,15 @@
 package org.java;
 
-public class NumeroPariDispari {
+import java.util.Scanner;
 
+public class NumeroPariDispari {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Inserisci un numero:");
+		String strNumber = in.nextLine();
+		int intNumber = Integer.valueOf(strNumber);
+		
+		in.close();
+	}
 }
