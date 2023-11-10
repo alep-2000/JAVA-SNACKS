43709f91d356f7686a0d2226905dc2c4fe86b18b
alep-2000 <alessiopiras056@gmail.com>
2023-11-10 15:18:08 +0100

    Ciclo casualmente nomi e cognomi per creare gli invitati e correzzione struttura con aggiunta main

--- src/org/java/InvitatiCasuali.java
+++ src/org/java/InvitatiCasuali.java
@@ -1,6 +1,20 @@
 package org.java;
 
+import java.util.Random;
+
 public class InvitatiCasuali {
-	String[] nomi = {"Alessio", "Giuseppe", "Francesco", "Laura", "Francesca", "Sara"};
-	String[] cognomi = {"Palmas", "Piras", "Cordeddu", "Lippi", "Santi", "Vadilonga"};
+	public static void main(String[] args) {
+		String[] nomi = {"Alessio", "Giuseppe", "Francesco", "Laura", "Francesca", "Sara"};
+		String[] cognomi = {"Palmas", "Piras", "Cordeddu", "Lippi", "Santi", "Vadilonga"};
+		
+		Random r = new Random();
+		int invitati = 10;
+		
+		for(int i=0; i < invitati; i++){
+			String nomeCasuale = nomi[r.nextInt(nomi.length)];
+			String cognomeCasuale = cognomi[r.nextInt(cognomi.length)];
+			
+			System.out.println("Invitato: " + nomeCasuale + " " + cognomeCasuale);
+		}
+	}
 }
