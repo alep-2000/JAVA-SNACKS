ccd55302c03292acf259b93fd825675fbe4ee0dc
alep-2000 <alessiopiras056@gmail.com>
2023-11-10 15:24:07 +0100

    Aggiunta somma dei numeri in posizione dispari e aggiunta del main inziale

--- src/org/java/ArrayNumeriInteri.java
+++ src/org/java/ArrayNumeriInteri.java
@@ -1,5 +1,17 @@
 package org.java;
 
 public class ArrayNumeriInteri {
-	int[] numeriInteri = {2, 5, 8, 10, 15, 20, 25, 30};
+	public static void main(String[] args) {
+		int[] numeriInteri = {2, 5, 8, 10, 15, 20, 25, 30};
+		
+		int sommaDispari = 0;
+		
+		for(int i=0; i < numeriInteri.length; i += 2){
+			
+			sommaDispari += numeriInteri[i];
+		}
+		
+		System.out.println("I numeri dispari sommati sono: " + sommaDispari);
+	}
+	
 }
