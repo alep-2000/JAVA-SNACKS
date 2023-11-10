79ce9b0fad74a1da9fd1592c78a5e32209d17fda
alep-2000 <alessiopiras056@gmail.com>
2023-11-10 15:28:56 +0100

    Richiesta di una stringa all'utente

--- src/org/java/StringaPalindroma.java
+++ src/org/java/StringaPalindroma.java
@@ -1,7 +1,14 @@
 package org.java;
 
+import java.util.Scanner;
+
 public class StringaPalindroma {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
+		System.out.print("Inserisci una stringa:");
+		String strWord = in.nextLine();
+		
+		in.close();
 	}
 }
