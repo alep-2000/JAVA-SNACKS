8cd1c6b07476c92713cf80284b95787eefe8d9dc
alep-2000 <alessiopiras056@gmail.com>
2023-11-10 17:26:38 +0100

    Chiedo la stringa all'utente e faccio un ciclo in cui controllo le componenti della stringa

--- src/org/java/CalcoloCaratteri.java
+++ src/org/java/CalcoloCaratteri.java
@@ -1,7 +1,33 @@
 package org.java;
 
+import java.util.Scanner;
+
 public class CalcoloCaratteri {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
+		System.out.print("Inserisci una stringa:");
+		String strWord = in.nextLine().toLowerCase().replaceAll("\\s", "");
+		int strLength = strWord.length();
+		
+		int caratteriAlfabetici = 0;
+        int numeri = 0;
+        int simboli = 0;
+        
+        for (int i = 0; i < strLength; i++) {
+            char c = strWord.charAt(i);
+
+            if (Character.isLetter(c)) {
+                caratteriAlfabetici++;
+            } else if (Character.isDigit(c)) {
+                numeri++;
+            } else {
+                simboli++;
+            }
+        }
+        
+        System.out.println("Caratteri: " + caratteriAlfabetici);
+        System.out.println("Numeri: " + numeri);
+        System.out.println("Simboli: " + simboli + "\n");
 	}
 }
