5679946a792c4d63aba19771c9fe03a0ebbc8faf
alep-2000 <alessiopiras056@gmail.com>
2023-11-12 06:45:21 +0100

    Creazione string e trasformazione in intero

--- src/org/java/StringaIntero.java
+++ src/org/java/StringaIntero.java
@@ -2,6 +2,9 @@ package org.java;
 
 public class StringaIntero {
 	public static void main(String[] args) {
+		String strNum = "8";
 		
+		char chrNum = strNum.charAt(0);
+		System.out.println(chrNum);
 	}
 }
