b10fb4d71b8f503e8d7f761e8335659608564aa7
alep-2000 <alessiopiras056@gmail.com>
2023-11-12 07:07:04 +0100

    Creazione string secondi e variabili ore, min, sec

--- src/org/java/SecondiInStringa.java
+++ src/org/java/SecondiInStringa.java
@@ -2,6 +2,11 @@ package org.java;
 
 public class SecondiInStringa {
 	public static void main(String[] args) {
+		int secondi = 1000;
+		
+		int ore = secondi / 3600;
+		int min = (secondi % 3600) / 60;
+		int sec = secondi % 60;
 		
 	}
 }
