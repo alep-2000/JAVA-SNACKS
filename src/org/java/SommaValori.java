71ddc82fc05cf206909e6594e16a2e29e1513c27
alep-2000 <alessiopiras056@gmail.com>
2023-11-12 21:18:28 +0100

    Richiesta dei due valori all'utente

--- src/org/java/SommaValori.java
+++ src/org/java/SommaValori.java
@@ -1,7 +1,19 @@
 package org.java;
 
+import java.util.Scanner;
+
 public class SommaValori {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
+		System.out.print("Inserisci il primo valore:");
+		String strValue1 = in.nextLine();
+		
+		System.out.print("Inserisci il secondo valore:");
+		String strValue2 = in.nextLine();
+
+		in.close();
+		
+		System.out.println("Primo valore: " + strValue1 + "\n" + "Secondo valore " + strValue2);
 	}
 }
