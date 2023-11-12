57b2436af7c58973ed15bd15adfbbecee075fddf
alep-2000 <alessiopiras056@gmail.com>
2023-11-12 08:36:37 +0100

    Conto i numeri pari e i numeri dispari presenti nell'array

--- src/org/java/ArrayDoppio.java
+++ src/org/java/ArrayDoppio.java
@@ -2,6 +2,17 @@ package org.java;
 
 public class ArrayDoppio {
 	public static void main(String[] args) {
+		int[] numeri = {1,4,9,10,45,60,120};
 		
-	}
+		int countPari = 0;
+        int countDispari = 0;
+        for (int numero : numeri) {
+            if (numero % 2 == 0) {
+                countPari++;
+            } else {
+                countDispari++;
+            }
+        }
+		
+    }
 }
