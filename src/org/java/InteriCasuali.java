e0307101ec6c693bedc7ad45e46bb09e6c8ec04a
alep-2000 <alessiopiras056@gmail.com>
2023-11-12 21:39:00 +0100

    Creo array di numeri casuali

--- src/org/java/InteriCasuali.java
+++ src/org/java/InteriCasuali.java
@@ -1,7 +1,16 @@
 package org.java;
 
+import java.util.Random;
+
 public class InteriCasuali {
 	public static void main(String[] args) {
+		Random r = new Random(1);
+		int[] array = new int[8];
 		
+		for (int i = 0; i < array.length; i++) {
+
+			array[i] = r.nextInt(0, 101);
+			System.out.println(i+1 + ": " + array[i]);
+		}
 	}
 }
