2d74231811ef2f97c2164de14f7b730f5ee2beb9
alep-2000 <alessiopiras056@gmail.com>
2023-11-12 07:50:42 +0100

    Creazione stringa e dichiarazione variabile d'appoggio

--- src/org/java/ScambioLettera.java
+++ src/org/java/ScambioLettera.java
@@ -2,6 +2,11 @@ package org.java;
 
 public class ScambioLettera {
 	public static void main(String[] args) {
+		String str1 = "Fantasmagorico";
+		int strLenght = str1.length();
+		
+		
+		String swapStr = "";
 		
 	}
 }
